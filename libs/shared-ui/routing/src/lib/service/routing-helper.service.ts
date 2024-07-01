eb3a6ae6437a76877df2ee6b05dc0b27c89839a4
DonaldMurillo <donald.murillo07@gmail.com>
2024-07-01 18:50:11 -0400

    updated logic for current url, added extra helper methods

--- libs/shared-ui/routing/src/lib/service/routing-helper.service.ts
+++ libs/shared-ui/routing/src/lib/service/routing-helper.service.ts
@@ -1,21 +1,50 @@
+import { DOCUMENT } from '@angular/common';
 import { Injectable, inject } from '@angular/core';
 import { takeUntilDestroyed, toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { Router } from '@angular/router';
-import { distinctUntilChanged, interval, map, } from 'rxjs';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map, startWith, } from 'rxjs';
 
 // TODO: DOCUMENT
 // TODO: UNIT TESTING
 @Injectable({ providedIn: 'root' })
 export class RoutingHelperService {
 
+	// PRIVATE DI
 	readonly #router = inject(Router);
+	readonly #document = inject(DOCUMENT);
 
-	readonly selectCurrentUrl = toSignal(interval(100).pipe(
-		takeUntilDestroyed(),
-		map(() => this.#router.url),
-		distinctUntilChanged()
+	readonly selectCurrentUrl = toSignal(this.#router.events.pipe(
+		filter((event) => event instanceof NavigationEnd),
+		map((event) => (event as NavigationEnd).urlAfterRedirects),
+		startWith(this.#router.url),
+		takeUntilDestroyed()
 	), { initialValue: this.#router.url });
 
 	readonly selectCurrentUrl$ = toObservable(this.selectCurrentUrl);
 
+	checkUrlPathSegment = (value: string) => this.#router.url.split('/').includes(value);
+
+	getReferer = () => this.#document.referrer;
+
+
+	// STATIC METHOD
+
+	static getFullTreeParams(
+		route: ActivatedRoute,
+		params: Record<string, string> = {},
+		isTopLevel = false,
+	): { [key: string]: string; } {
+		if (route.parent && !isTopLevel)
+			return RoutingHelperService.getFullTreeParams(route.parent);
+		else {
+			if (route) {
+				params = { ...params, ...route?.snapshot?.params };
+				route.children.forEach(
+					(c) => (params = RoutingHelperService.getFullTreeParams(c, params, true)),
+				);
+				return params;
+			}
+			return params;
+		}
+	}
 }
